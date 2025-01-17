56d83bbbda11395d0dd0b654cf7e71a0a8eb4857
daviz616 <david.castejon616@gmail.com>
2025-01-17 09:14:28 +0100

    Lenguaje de marcas

--- Programacion/Punteros/Práctica6_DavidCastejón.c
+++ Programacion/Punteros/Práctica6_DavidCastejón.c
@@ -30,212 +30,49 @@ int currentBooks = 40;  // Número actual de libros en el catálogo.
 
 // Imprime los detalles de un libro específico.
 // Uso de un puntero constante 'const Book *' para evitar modificar el contenido del libro.
-void printBook(const Book * OneBook){
-    printf("%d, %s, %s, %.2f, %d, %d. \n",  
-        OneBook->id, OneBook->title,
-        OneBook->author,OneBook->price,
-        OneBook->genre,OneBook->quantity);
-
-}
+void printBook(const Book * OneBook);
 
 // Imprime los detalles de todos los libros del catálogo.
 // Se pasa un puntero constante al inicio del array para garantizar que no se modifique.
-void printAllBooks( const Book * catalog){
-    for(int i = 0; i < currentBooks ; i++){
-        printBook(&catalog[i]);
-        }
-}
+void printAllBooks( const Book * catalog);
 
 // Busca e imprime un libro por su ID introducido por el usuario.
 // Uso de paso por referencia para evitar copiar todo el catálogo.
-void printIDBook(const Book * catalog){
-    // La función recibe un puntero constante a un array de libros 'catalog'.
-    // Esto significa que se trabaja directamente con la dirección de memoria del catálogo,
-    // en lugar de hacer una copia completa de los datos. 
-    // Además, el 'const' asegura que esta función no puede modificar los datos del catálogo.
-    int id;
-    printf("Introduce the ID of the book that you want: ");
-    scanf("%d", &id);
-    for(int i = 0; i < MAX_BOOKS; i++){
-        if (catalog[i].id == id){
-            printBook(&catalog[i]);
-        }
-    }
-}// Si el ID no coincide con ningún libro, simplemente no se realiza ninguna acción.
-
+void printIDBook(const Book * catalog);
 
 // Función alternativa para imprimir un libro dado su ID, pasando parámetros explícitos (ID de búsqueda).
 // Modifica directamente el puntero 'catalog' al ser un puntero no constante.
-void printIDBook_nq(Book * catalog,const int search){
-    for(int i = 0; i < MAX_BOOKS; i++ , catalog++){
-        if (catalog->id == search){
-           printBook(catalog);
-        }
-    }
-}
+void printIDBook_nq(Book * catalog,const int search);
 
 // Incrementa el stock de un libro específico.
 // Paso por referencia (no constante) para modificar el catálogo directamente.
-void printStockIDQuantity(Book * catalog){
-    int id;
-    printf("Introduce the ID of the book that you want to increase: ");
-    scanf("%d", &id);
-    int PlusBook; 
-    printf("Introduce the quantity of the book to increase ");
-    scanf("%d",&PlusBook);
-
-    for(int i = 0; i < MAX_BOOKS; i++){
-        if (catalog[i].id == id){
-            catalog[i].quantity += PlusBook;
-            printf("Stock aumented\n ");
-            printBook(&catalog[i]);
-            return;
-        }
-    }
-}
+void printStockIDQuantity(Book * catalog);
 
 // Función alternativa para aumentar el stock de un libro dado su ID y cantidad.
 // Usa puntero no constante para poder modificar el catálogo directamente.
-void printStockIDQuantity_nq(Book * catalog, const int id, const int addquantity){
-    for(int i = 0; i < MAX_BOOKS; i++, catalog++){
-        if (catalog->id == id){
-            catalog->quantity += addquantity;
-            printf("Stock aumented\n ");
-            printBook(catalog);
-            return;
-        }
-    }
-}
+void printStockIDQuantity_nq(Book * catalog, const int id, const int addquantity);
 
 // Filtra e imprime los libros de una categoría específica.
 // Modifica el uso del catálogo para extraer libros de un género concreto.
-void printBookCategory(Book * catalog){
-    int category_number; 
-    printf("Please put the number of the category u want to see(0-FICTION,1-NON-FICTION,2-POETRY,3-THEATER,4-ESSAY:");      
-    scanf("%d", &category_number);   
-    if(category_number < 5){
-        printf("Books in that category:\n");
-        for(int i = 0; i < MAX_BOOKS; i++){
-              // Comapra el género del libro con el número introducido por el usuario,
-            // si coinciden, se imprime el libro correspondiente.
-              if (catalog[i].genre == category_number) {
-                    printBook(&catalog[i]);
-            }
-        
-            }
-        }
-    }
- 
- // Función alternativa que filtra e imprime libros según su género.
-void printcategorynum(Book * catalog, const int category){
-        for(int i = 0; i < MAX_BOOKS; i++, catalog++){
-            //Verifica si el género del libro coincide con el género proporcionado como parámetro.
-              if (catalog->genre == category) {
-                    printBook(catalog);
-            }
-        
-    }
- }
-    
+void printBookCategory(Book * catalog);
+
+// Función alternativa que filtra e imprime libros según su género.
+void printcategorynum(Book * catalog, const int category);
+
  // Inicializa un libro con sus detalles (ID, título, autor, precio, categoría y cantidad).
 // Utiliza 'strcpy' para copiar cadenas de texto en los campos correspondientes del libro.   
-void initializeBook(Book *newBook, int id, const char *title, const char *author, float price, Category genre, int quantity) {
-    newBook->id = id;
-    strcpy(newBook->title, title);
-    strcpy(newBook->author, author);
-    newBook->price = price;
-    newBook->genre = genre;
-    newBook->quantity = quantity;
-}
+void initializeBook(Book *newBook, int id, const char *title, const char *author, float price, Category genre, int quantity);
 
 //Añade nuevos libros al catálogo mediante realocación dinámica de memoria.
-void addBook(Book ** catalog){//Doble puntero porque lo que quiero actualizar
-    // es la dirección de memoria donde se guarda la dirección de memoria del 
-    //catalogo para que se actualice con el realloc
-
-        int newquant;
-        printf("Enter the number of books to add: ");
-        scanf("%d",&newquant);
-
-    // Actualizar el total de libros
-    currentBooks += newquant;
-    *catalog = (Book *)realloc(*catalog, currentBooks * sizeof(Book)); 
-        if (*catalog == NULL) {
-        printf("Memory allocation failed!\n");
-        return;
-    }
-
-    // Rellenar los detalles de los nuevos libros en el catálogo.
-    for(int i = currentBooks - newquant; i < currentBooks; i++){
-        
-        int id_new;
-         printf("Enter the id: ");
-        scanf("%d",&id_new);
-
-        char title_new[80];
-        printf("Enter the title: ");
-        scanf(" %s",title_new);
-
-        char author_new[60];
-         printf("Enter the author: ");
-        scanf(" %s",author_new);
-
-        float price_new;
-         printf("Enter the price: ");
-        scanf("%f",&price_new);
-
-        int genre_new;
-         printf("Enter the nº of the genre: ");
-        scanf("%d",&genre_new);
-
-        int quantity_new;
-         printf("Enter the quantity or stock: ");
-        scanf("%d",&quantity_new);
-        
-        // Inicializa el libro con los detalles obtenidos y lo agrega al catálogo.
-        initializeBook(&(*catalog)[i], id_new, title_new, author_new, price_new, (Category)genre_new, quantity_new);
-        }
-}
+void addBook(Book ** catalog);
 
 // Imprime todos los libros de un autor específico.
 // Si no se encuentra ningún libro del autor, muestra un mensaje indicando la ausencia.
-void printBooksByAuthor(const Book *catalog, const char *author) {
-    int found = 0; // Variable para comprobar si se encontraron libros del autor.
-
-    for (int i = 0; i < currentBooks; i++) {
-        // Si el autor del libro coincide con el autor proporcionado.
-        if (strcmp(catalog[i].author, author) == 0) {
-            // Muestra el libro si hay coincidencia.
-            printBook(&catalog[i]);
-            found = 1; //Se encuentra al menos un libro del autor.
-        }
-    }
-
-    // Si no se encontraron libros:
-    if (!found) {
-        printf("No books found by author %s.\n", author);
-    }
-}
+void printBooksByAuthor(const Book *catalog, const char *author);
 
 // Muestra el uso del programa, detallando las acciones que el usuario puede realizar con los diferentes comandos.
-void printUsage() {
-    printf("Usage of the library program(assuming you compile the file as library):\n");
-    printf("\t./library all\n");
-    printf("\t\tTo run the full functionality of the program.\n");
-    printf("\t./library show\n");
-    printf("\t\tTo show the entire book catalog.\n");
-    printf("\t./library show ID(number)\n");
-    printf("\t\tTo show the book with the given ID.\n");
-    printf("\t./library category CATEGORY(number)\n");
-    printf("\t\tTo show books in a specific category (0-FICTION, 1-NON_FICTION, 2-POETRY, 3-THEATER, 4-ESSAY).\n");
-    printf("\t./library author (name with quotes)\n");
-    printf("\t\tTo show books by a specific author.\n");
-    printf("\t./library stock ID QUANTITY\n");
-    printf("\t\tTo increase the stock of a book by the given ID and quantity.\n");
-    printf("\t./library add\n");
-    printf("\t\tTo add a book with the information\n");
+void printUsage();
 
-}
 
 //argc: número de argumentos recibidos.
 //argv: array de cadenas de texto
@@ -341,4 +178,189 @@ int main(int argc, char **argv){
     free(catalog);
 
     return 0;
+}
+
+void printBook(const Book * OneBook){
+    printf("%d, %s, %s, %.2f, %d, %d. \n",  
+        OneBook->id, OneBook->title,
+        OneBook->author,OneBook->price,
+        OneBook->genre,OneBook->quantity);
+}
+
+void printAllBooks( const Book * catalog){
+    for(int i = 0; i < currentBooks ; i++){
+        printBook(&catalog[i]);
+        }
+}
+
+void printIDBook(const Book * catalog){
+    // La función recibe un puntero constante a un array de libros 'catalog'.
+    // Esto significa que se trabaja directamente con la dirección de memoria del catálogo,
+    // en lugar de hacer una copia completa de los datos. 
+    // Además, el 'const' asegura que esta función no puede modificar los datos del catálogo.
+    int id;
+    printf("Introduce the ID of the book that you want: ");
+    scanf("%d", &id);
+    for(int i = 0; i < MAX_BOOKS; i++){
+        if (catalog[i].id == id){
+            printBook(&catalog[i]);
+        }
+    }
+}// Si el ID no coincide con ningún libro, simplemente no se realiza ninguna acción.
+
+void printIDBook_nq(Book * catalog,const int search){
+    for(int i = 0; i < MAX_BOOKS; i++ , catalog++){
+        if (catalog->id == search){
+           printBook(catalog);
+        }
+    }
+}
+
+void printStockIDQuantity(Book * catalog){
+    int id;
+    printf("Introduce the ID of the book that you want to increase: ");
+    scanf("%d", &id);
+    int PlusBook; 
+    printf("Introduce the quantity of the book to increase ");
+    scanf("%d",&PlusBook);
+
+    for(int i = 0; i < MAX_BOOKS; i++){
+        if (catalog[i].id == id){
+            catalog[i].quantity += PlusBook;
+            printf("Stock aumented\n ");
+            printBook(&catalog[i]);
+            return;
+        }
+    }
+}
+
+void printStockIDQuantity_nq(Book * catalog, const int id, const int addquantity){
+    for(int i = 0; i < MAX_BOOKS; i++, catalog++){
+        if (catalog->id == id){
+            catalog->quantity += addquantity;
+            printf("Stock aumented\n ");
+            printBook(catalog);
+            return;
+        }
+    }
+}
+
+void printBookCategory(Book * catalog){
+    int category_number; 
+    printf("Please put the number of the category u want to see(0-FICTION,1-NON-FICTION,2-POETRY,3-THEATER,4-ESSAY:");      
+    scanf("%d", &category_number);   
+    if(category_number < 5){
+        printf("Books in that category:\n");
+        for(int i = 0; i < MAX_BOOKS; i++){
+              // Comapra el género del libro con el número introducido por el usuario,
+            // si coinciden, se imprime el libro correspondiente.
+              if (catalog[i].genre == category_number) {
+                    printBook(&catalog[i]);
+            }
+        
+            }
+        }
+    }
+
+void printcategorynum(Book * catalog, const int category){
+        for(int i = 0; i < MAX_BOOKS; i++, catalog++){
+            //Verifica si el género del libro coincide con el género proporcionado como parámetro.
+              if (catalog->genre == category) {
+                    printBook(catalog);
+            }
+        
+    }
+ }
+ 
+void initializeBook(Book *newBook, int id, const char *title, const char *author, float price, Category genre, int quantity) {
+    newBook->id = id;
+    strcpy(newBook->title, title);
+    strcpy(newBook->author, author);
+    newBook->price = price;
+    newBook->genre = genre;
+    newBook->quantity = quantity;
+}
+
+void addBook(Book ** catalog){//Doble puntero porque lo que quiero actualizar
+    // es la dirección de memoria donde se guarda la dirección de memoria del 
+    //catalogo para que se actualice con el realloc
+
+        int newquant;
+        printf("Enter the number of books to add: ");
+        scanf("%d",&newquant);
+
+    // Actualizar el total de libros
+    currentBooks += newquant;
+    *catalog = (Book *)realloc(*catalog, currentBooks * sizeof(Book)); 
+        if (*catalog == NULL) {
+        printf("Memory allocation failed!\n");
+        return;
+    }
+
+    // Rellenar los detalles de los nuevos libros en el catálogo.
+    for(int i = currentBooks - newquant; i < currentBooks; i++){
+        
+        int id_new;
+         printf("Enter the id: ");
+        scanf("%d",&id_new);
+
+        char title_new[80];
+        printf("Enter the title: ");
+        scanf(" %s",title_new);
+
+        char author_new[60];
+         printf("Enter the author: ");
+        scanf(" %s",author_new);
+
+        float price_new;
+         printf("Enter the price: ");
+        scanf("%f",&price_new);
+
+        int genre_new;
+         printf("Enter the nº of the genre: ");
+        scanf("%d",&genre_new);
+
+        int quantity_new;
+         printf("Enter the quantity or stock: ");
+        scanf("%d",&quantity_new);
+        
+        // Inicializa el libro con los detalles obtenidos y lo agrega al catálogo.
+        initializeBook(&(*catalog)[i], id_new, title_new, author_new, price_new, (Category)genre_new, quantity_new);
+        }
+}
+void printBooksByAuthor(const Book *catalog, const char *author) {
+    int found = 0; // Variable para comprobar si se encontraron libros del autor.
+
+    for (int i = 0; i < currentBooks; i++) {
+        // Si el autor del libro coincide con el autor proporcionado.
+        if (strcmp(catalog[i].author, author) == 0) {
+            // Muestra el libro si hay coincidencia.
+            printBook(&catalog[i]);
+            found = 1; //Se encuentra al menos un libro del autor.
+        }
+    }
+
+    // Si no se encontraron libros:
+    if (!found) {
+        printf("No books found by author %s.\n", author);
+    }
+}
+
+void printUsage() {
+    printf("Usage of the library program(assuming you compile the file as library):\n");
+    printf("\t./library all\n");
+    printf("\t\tTo run the full functionality of the program.\n");
+    printf("\t./library show\n");
+    printf("\t\tTo show the entire book catalog.\n");
+    printf("\t./library show ID(number)\n");
+    printf("\t\tTo show the book with the given ID.\n");
+    printf("\t./library category CATEGORY(number)\n");
+    printf("\t\tTo show books in a specific category (0-FICTION, 1-NON_FICTION, 2-POETRY, 3-THEATER, 4-ESSAY).\n");
+    printf("\t./library author (name with quotes)\n");
+    printf("\t\tTo show books by a specific author.\n");
+    printf("\t./library stock ID QUANTITY\n");
+    printf("\t\tTo increase the stock of a book by the given ID and quantity.\n");
+    printf("\t./library add\n");
+    printf("\t\tTo add a book with the information\n");
+
 }
